cd18192a68833becb50727e52c5d6d5d613ab6ed
myclassinfo <34704224+myclassinfo@users.noreply.github.com>
2018-01-04 21:24:53 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const staticAssets = [
+	'./',
+	'./index.html',
+	'./login.html',
+	'./logo.png',
+	'https://fonts.googleapis.com/icon?family=Material+Icons',
+	'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css',
+	'https://code.jquery.com/jquery-3.2.1.min.js',
+	'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js'
+];
+
+self.addEventListener('install', async event=>{
+	const cache = await caches.open('lib-static');
+	cache.addAll(staticAssets);
+
+});
+
+self.addEventListener('fetch', event=>{
+	const req = event.request;
+	event.respondWith(cacheFirst(req));
+});
+
+async function cacheFirst(req){
+	const cachedResponse = await caches.match(req);
+	return cachedResponse || fetch(req);
+}
